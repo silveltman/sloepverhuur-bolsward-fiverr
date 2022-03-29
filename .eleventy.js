8813879452b814df96e7d55ade890f2695d6d918
Sil Veltman <silveltman@hotmail.com>
2022-03-29 17:34:35 +0200

    c

--- .eleventy.js
+++ .eleventy.js
@@ -72,6 +72,11 @@ module.exports = function(eleventyConfig) {
     return found
   });
 
+    // Filters collection by a frontmatter field
+    eleventyConfig.addFilter("sortByFrontmatter", (collection, key) => {
+      return collection.sort((a, b) => (a.data[key] > b.data[key]) ? 1 : -1)
+    })
+
   // Returns each collection item which fileSlug is included in an array 
   eleventyConfig.addFilter("filterBySlug", function(collection, slugList) {
     const found = collection.filter(item => {
